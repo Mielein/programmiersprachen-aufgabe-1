bbae591b62442fe583843e0eab5f2ccf65fc0474
Mielein <marie.trojan@uni-weimar.de>
2020-05-07 00:02:19 +0200

    mile_to_kilometer implemented

---
+++ source/mile_to_kilometer.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <iostream>
+
+double mile_to_kilometer(double mile){
+ double kilometer = mile*1.60934;
+ return kilometer;
+}
+
+TEST_CASE("compile mile_to_kilometer ","[mile_to_kilometer]"){
+  REQUIRE(mile_to_kilometer(0)==Approx(0));
+  REQUIRE(mile_to_kilometer(1)==Approx(1.60934));
+  REQUIRE(mile_to_kilometer(1.2)==Approx(0.72));
+}
+
+int main(int argc, char* argv[])
+{
+ double miles;
+ std::cout<<"Enter miles you like to convert"<<std::endl;
+ std::cin>>miles;
+ std::cout<<miles<<" miles are "<<mile_to_kilometer(miles)<<" kilometers"<<std::endl;
+ return Catch::Session().run(argc, argv);
+}
+
+
