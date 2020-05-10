0d1b5f005ed4e678f549bcd09ae7149e0d9c157f
Mielein <marie.trojan@uni-weimar.de>
2020-05-10 18:21:12 +0200

    quersumme updated

--- source/quersumme.cpp
+++ source/quersumme.cpp
@@ -3,26 +3,39 @@
 #include <catch.hpp>
 #include <cmath>
 
-int sum(int zahl){
+int sum(int eingabe){
+ int zahl = eingabe;
  int qsum = 0;
- while (0 != zahl){
-  qsum= qsum+(zahl%10);
-  zahl = zahl/10;
+ 
+
+ if(eingabe>0){
+  while (0 != zahl){
+   qsum= qsum+(zahl%10);
+   zahl = zahl/10;
+  }
+  return qsum;
  }
- return qsum;
+ else{
+   return -1;
+ }
 }
 
 TEST_CASE("checksum is computed","[sum]"){
  REQUIRE(sum(123)==6);
  REQUIRE(sum(234)==9);
- REQUIRE(sum(890)==9);
  REQUIRE(sum(121067)==17);
+ REQUIRE(sum(0));
 }
 
 int main(int argc, char* argv[]){
   int x;
-  std::cout<<"Type a number to get the checksum"<<std::endl;
+  std::cout<<"Type a non-negative number to get the checksum of it"<<std::endl;
   std::cin>>x;
+  if(x<=0){
+    std::cout<<"I said non-negative numbers"<<std::endl;
+    }
+  else{
   std::cout<<"the checksum of the number "<<x<<" is "<<sum(x)<<std::endl;
+  }
  return Catch::Session().run(argc, argv);
 }
