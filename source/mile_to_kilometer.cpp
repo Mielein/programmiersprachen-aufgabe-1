b542ed37bd55df7ef12c2f2a6390c43345b4b264
Mielein <marie.trojan@uni-weimar.de>
2020-05-11 12:01:45 +0200

    mile_to_kilometer updated

--- source/mile_to_kilometer.cpp
+++ source/mile_to_kilometer.cpp
@@ -10,7 +10,7 @@ double mile_to_kilometer(double mile){
 TEST_CASE("compile mile_to_kilometer ","[mile_to_kilometer]"){
   REQUIRE(mile_to_kilometer(0)==Approx(0));
   REQUIRE(mile_to_kilometer(1)==Approx(1.60934));
-  REQUIRE(mile_to_kilometer(1.2)==Approx(0.72));
+  REQUIRE(mile_to_kilometer(-1)==Approx(-1.60934));
 }
 
 int main(int argc, char* argv[])
@@ -18,7 +18,12 @@ int main(int argc, char* argv[])
  double miles;
  std::cout<<"Enter miles you like to convert"<<std::endl;
  std::cin>>miles;
+ if(miles<1){
+   std::cout<<"please use a non-negative number"<<std::endl;
+ }
+ else{
  std::cout<<miles<<" miles are "<<mile_to_kilometer(miles)<<" kilometers"<<std::endl;
+ }
  return Catch::Session().run(argc, argv);
 }
 
