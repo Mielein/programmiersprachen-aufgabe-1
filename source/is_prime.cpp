e52729ecf316d1415f717f4217a339f1febb2246
Mielein <marie.trojan@uni-weimar.de>
2020-05-11 11:46:27 +0200

    is_prime updated

--- source/is_prime.cpp
+++ source/is_prime.cpp
@@ -1,30 +1,44 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
 #include <iostream>
 #include <cmath>
 #include <algorithm>
 
 bool is_prime(int x){
- bool prime=true;
+bool prime=true;
+ if(x<2){
+   prime=false;
+ }
  for(int i=2;i<=x/2; i++){
   if(x%i == 0){
-   prime=false;
+    prime=false;
   }
  }
  return prime;
+}
+
+TEST_CASE("compile is_prime","[is_prime]"){
+  REQUIRE(is_prime(-3)==false);
+  REQUIRE(is_prime(4)==false);
+  REQUIRE(is_prime(7)==true);
 } 
 
-int main(){
+int main(int argc, char* argv[]){
  int number;
-
  std::cout<<"Gib eine Zahl ein:"<<std::endl;
  std::cin>>number;
-
- if(is_prime(number)==true){
-  std::cout<<number<<" ist eine Primzahl \n"<<std::endl;
+ if(number>1){
+   if(is_prime(number)==true){
+     std::cout<<number<<" ist eine Primzahl \n"<<std::endl;
+   }
+   else{
+     std::cout<<number<<" ist keine Primzahl \n"<<std::endl;
+   }
  }
  else{
-  std::cout<<number<<" ist keine Primzahl \n"<<std::endl;
- }
- return 0;
+   std::cout<<"choose a number bigger than one"<<std::endl;
+ } 
+ return Catch::Session().run(argc, argv);
 }
 
 
